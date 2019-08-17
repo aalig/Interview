8c799364855cdbd5635acdab88b8828d346a7017
Abdurahman Gugssa <abdu.mum@gmail.com>
2019-08-16 18:12:22 -0700

    Add project files.

---
+++ Interview/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using LinqTutorial;
+
+namespace Interview
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+            var cheapBooks = books
+                .Where(b => b.Price < 10)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title);
+
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book);
+            }
+
+            var maxPrice = books.Max(b => b.Price);
+            var minPrice = books.Min(b => b.Price);
+            var totalPrices = books.Sum(b => b.Price);
+
+            Console.WriteLine("Max Price: {0}, Min Price: {1}, Total Price: {2}", maxPrice, minPrice, totalPrices);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LinqTutorial/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LinqTutorial
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "HTML Crash Course", Price = 5.99f},
+                new Book() {Title = "CSS Crash Course", Price = 9.99f},
+                new Book() {Title = "Basic CSharp Course", Price = 9.99f},
+                new Book() {Title = "Intermediate CSharp Course", Price = 14.99f},
+                new Book() {Title = "Advanced CSharp Course", Price = 19.99f}
+            };
+        }
+    }
+}
